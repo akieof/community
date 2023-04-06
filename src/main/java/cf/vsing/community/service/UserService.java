e17016d28e7d3034e48241f9afd46e2109901925
akieof <Regist_NO_020366@outlook.com>
2023-04-06 22:13:48 +0800

    2023/4/3

--- src/main/java/cf/vsing/community/service/UserService.java
+++ src/main/java/cf/vsing/community/service/UserService.java
@@ -1,13 +1,17 @@
 package cf.vsing.community.service;
 
+import cf.vsing.community.dao.LoginTicketMapper;
 import cf.vsing.community.dao.UserMapper;
+import cf.vsing.community.entity.LoginTicket;
 import cf.vsing.community.entity.User;
 import cf.vsing.community.util.CommunityUtil;
 import cf.vsing.community.util.MailClientUtil;
 import cf.vsing.community.util.StatusUtil;
+import jakarta.servlet.http.HttpSession;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.server.Cookie;
 import org.springframework.stereotype.Service;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
@@ -17,9 +21,11 @@ import java.util.HashMap;
 import java.util.Map;
 
 @Service
-public class UserService implements StatusUtil{
+public class UserService implements StatusUtil {
     @Autowired
     private UserMapper userMapper;
+    @Autowired
+    private LoginTicketMapper loginTicketMapper;
     @Autowired
     private MailClientUtil mailClientUtil;
     @Autowired
@@ -27,70 +33,132 @@ public class UserService implements StatusUtil{
     @Value("${project.domain}")
     private String domain;
 
-    public User findUserById(int id){
+
+    public User findUserById(int id) {
         return userMapper.selectById(id);
     }
 
-    public Map<String,Object> register(User user){
-        Map<String, Object> map=new HashMap<>();
+    /**
+     * 用户注册
+     *
+     * @param user
+     * @return
+     */
+    public Map<String, Object> register(User user) {
+        Map<String, Object> map = new HashMap<>();
 
         //非法值处理
-        if(user==null){
-            throw  new IllegalArgumentException("参数不能为空");
+        if (user == null) {
+            throw new IllegalArgumentException("参数不能为空");
         }
 
         //空值处理
-        if(StringUtils.isBlank(user.getUsername())){
-            map.put("usernameMsg","账号不能为空");
+        if (StringUtils.isBlank(user.getUsername())) {
+            map.put("usernameMsg", "账号不能为空");
             return map;
         }
-        if(StringUtils.isBlank(user.getPassword())){
-            map.put("passwordMsg","密码不能为空");
+        if (StringUtils.isBlank(user.getPassword())) {
+            map.put("passwordMsg", "密码不能为空");
             return map;
         }
-        if(StringUtils.isBlank(user.getEmail())){
-            map.put("emailMsg","邮箱不能为空");
+        if (StringUtils.isBlank(user.getEmail())) {
+            map.put("emailMsg", "邮箱不能为空");
             return map;
         }
 
-        if(userMapper.selectByName(user.getUsername())!=null){
-            map.put("usernameMsg","用户已存在！");
+        //重复注册判断
+        if (userMapper.selectByName(user.getUsername()) != null) {
+            map.put("usernameMsg", "用户已存在！");
             return map;
         }
-        if(userMapper.selectByEmail(user.getEmail())!=null){
-            map.put("emailMsg","邮箱已被注册！");
+        if (userMapper.selectByEmail(user.getEmail()) != null) {
+            map.put("emailMsg", "邮箱已被注册！");
             return map;
         }
-        user.setSalt(CommunityUtil.generateUUID().substring(0,5));
-        user.setPassword(CommunityUtil.md5(user.getPassword()+user.getSalt()));
+
+        //用户信息初始化
+        user.setSalt(CommunityUtil.generateUUID().substring(0, 5));
+        user.setPassword(CommunityUtil.md5(user.getPassword() + user.getSalt()));
         user.setType(0);
         user.setStatus(0);
         user.setActivationCode(CommunityUtil.generateUUID());
-        user.setHeaderUrl("http://images.nowcoder.com/head/1.png");
+        user.setHeaderUrl("http://images.nowcoder.com/head/1t.png");
         user.setCreateTime(new Date());
-        userMapper.insertUser(user);
 
-        String activationUrl=domain+"/activation/"+user.getId()+"/"+user.getActivationCode();
-        Context context=new Context();
-        context.setVariable("email",user.getEmail());
-        context.setVariable("url",activationUrl);
-        String emailText=templateEngine.process("/mail/activation",context);
-        mailClientUtil.sendMail(user.getEmail(),"Vsing邮箱验证",emailText);
+        userMapper.insertUser(user);
+        String activationUrl = domain + "/activation/" + user.getId() + "/" + user.getActivationCode();
+
+        //发送激活邮件
+        Context context = new Context();
+        context.setVariable("email", user.getEmail());
+        context.setVariable("url", activationUrl);
+        String emailText = templateEngine.process("/mail/activation", context);
+        mailClientUtil.sendMail(user.getEmail(), "Vsing邮箱验证", emailText);
 
         return map;
     }
 
-    public int activation(int userId,String activationCode){
-        User user=userMapper.selectById(userId);
+    /**
+     * 用户登录
+     *
+     * @param username
+     * @param password
+     * @param expiredSeconds
+     * @return
+     */
+    public Map<String, String> login(String username, String password, int expiredSeconds) {
+        Map<String, String> msg = new HashMap<>();
 
-        if(user==null){
-           return ACTIVATION_NULL;
-        } else if(user.getStatus()==1){
+        //空值处理
+        if (StringUtils.isBlank(username)) {
+            msg.put("usernameMsg", "账号不能为空");
+            return msg;
+        }
+        if (StringUtils.isBlank(password)) {
+            msg.put("passwordMsg", "密码不能为空");
+            return msg;
+        }
+
+        //验证用户
+        User user = userMapper.selectByName(username);
+        if (user == null) {
+            msg.put("usernameMsg", "用户不存在，请先注册！");
+            return msg;
+        }
+        if (user.getStatus() == 0) {
+            msg.put("usernameMsg", "用户未激活，请先激活！");
+            return msg;
+        }
+        //比对密码
+        password = CommunityUtil.md5(password + user.getSalt());
+        if (!user.getPassword().equals(password)) {
+            msg.put("passwordMsg", "密码错误，请重新输入！");
+            return msg;
+        }
+        //保存凭证
+        LoginTicket ticket = new LoginTicket(user.getId(), CommunityUtil.generateUUID(), 0, new Date(System.currentTimeMillis() + expiredSeconds * 1000));
+        loginTicketMapper.insetTicket(ticket);
+        msg.put("ticket", ticket.getTicket());
+        return msg;
+    }
+
+    public void logout(String ticket){
+        loginTicketMapper.updateStatus(ticket,1);
+    }
+    public LoginTicket getLoginTicket(String ticket){
+        return loginTicketMapper.selectTicketByTicket(ticket);
+    }
+
+    public int activation(int userId, String activationCode) {
+        User user = userMapper.selectById(userId);
+        if (user == null) {
+            return ACTIVATION_NULL;
+        } else if (user.getStatus() == 1) {
             return ACTIVATION_REPEAT;
         } else if (user.getActivationCode().equals(activationCode)) {
-            userMapper.updateStatus(userId,1);
+            userMapper.updateStatus(userId, 1);
             return ACTIVATION_OK;
-        }else {
+        } else {
             return ACTIVATION_WRONG;
         }
     }
--- src/test/java/cf/vsing/community/MapperTest.java
+++ src/test/java/cf/vsing/community/MapperTest.java
@@ -1,8 +1,10 @@
 package cf.vsing.community;
 
 import cf.vsing.community.dao.DiscussPostMapper;
+import cf.vsing.community.dao.LoginTicketMapper;
 import cf.vsing.community.dao.UserMapper;
 import cf.vsing.community.entity.DiscussPost;
+import cf.vsing.community.entity.LoginTicket;
 import cf.vsing.community.entity.User;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -11,6 +13,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.Date;
 import java.util.List;
 
 @RunWith(SpringRunner.class)
@@ -21,16 +24,76 @@ public final class MapperTest {
     private UserMapper userMapper;
     @Autowired
     private DiscussPostMapper discussPostMapper;
+    @Autowired
+    private LoginTicketMapper loginTicketMapper;
+    @Test
+    public void  userInsert(){
+
+    }
 
     @Test
-    public void  testSelect(){
-        List<DiscussPost> list=discussPostMapper.selectDiscussPost(0,0,10);
-        int row=discussPostMapper.selectDiscussPostPostRows(0);
-        List<DiscussPost> post=discussPostMapper.selectDiscussPost(102,0,10);
+    public void  userSelect(){
+
+    }
+
+    @Test
+    public void  discuss_post(){
+        int userId=1,
+            offset=1,
+            limit=1;
+        try{
+            int result=discussPostMapper.selectDiscussPostPostRows(0);
+            System.out.println("selectDiscussPostPostRows 方法成功执行!\n参数:userId=0"+"\n结果:" + result);
+        }catch (Exception e){
+            System.out.println("selectDiscussPostPostRows 方法执行失败,错误信息如下为:\n" + e.getMessage());
+        }
+
+        try{
+            int result=discussPostMapper.selectDiscussPostPostRows(userId);
+            System.out.println("selectDiscussPostPostRows 方法成功执行!\n参数:userId="+userId+"\n结果:" + result);
+        }catch (Exception e){
+            System.out.println("selectDiscussPostPostRows 方法执行失败,错误信息如下为:\n" + e.getMessage());
+        }
+
+        try{
+            List<DiscussPost> result=discussPostMapper.selectDiscussPost(0,offset,limit);
+            System.out.println("selectDiscussPost 方法成功执行!\n参数:userId=0"+"\t+offset="+offset+"\tlimit="+offset+"\n结果:" + result);
+        }catch (Exception e){
+            System.out.println("selectDiscussPost 方法执行失败,错误信息如下为:\n" + e.getMessage());
+        }
+
+        try{
+            List<DiscussPost> result=discussPostMapper.selectDiscussPost(userId,offset,limit);
+            System.out.println("selectDiscussPost 方法成功执行!\n参数:userId="+userId+"\t+offset="+offset+"\tlimit="+offset+"\n结果:" + result);
+        }catch (Exception e){
+            System.out.println("selectDiscussPost 方法执行失败,错误信息如下为:\n" + e.getMessage());
+        }
+
+    }
 
 
-        System.out.println("Row="+row);
-        System.out.println("post="+post);
-        System.out.println("list"+list);
+    @Test
+    public void  login_ticket(){
+
+        //Test Data
+        LoginTicket loginTicket=new LoginTicket(2,"abc",0,new Date());
+
+        //insetTicket(LoginTicket ticket);
+        if(loginTicketMapper.insetTicket(loginTicket)==1)
+            System.out.println("insetTicket 方法成功执行,插入信息为:" + loginTicket.toString());
+        else
+            System.out.println("insetTicket 方法执行失败");
+
+        //selectTicketByTicket(Sting ticket)
+        if(loginTicketMapper.selectTicketByTicket(loginTicket.getTicket())!=null)
+            System.out.println("selectTicketByTicket 方法成功执行,结果为："+loginTicketMapper.selectTicketByTicket(loginTicket.getTicket()).toString());
+        else
+            System.out.println("selectTicketByTicket 方法执行失败");
+
+        //updateStatus(Sting ticket,int status)
+        if(loginTicketMapper.updateStatus("abc",1)==1)
+            System.out.println("updateStatus 方法成功执行，结果为："+loginTicketMapper.selectTicketByTicket(loginTicket.getTicket()).toString());
+        else
+            System.out.println("updateStatus 方法成功执行失败");
     }
 }
